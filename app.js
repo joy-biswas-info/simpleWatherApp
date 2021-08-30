89c5e955771a7908bcc97b6cef0cd1a02c47387d
joy biswas <contact.joybiswas@gmail.com>
2021-08-30 15:18:59 +0600

    Simple Weather app

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+function getInput() {
+    const input = document.getElementById('input');
+const inputText = input.value;
+    return inputText;
+}
 const getWeather = () => {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=manikgonj&units=metric&appid=ab68c3c6730b171480e2ed2eeea340fc')
+    getInput();
+    let url =`https://api.openweathermap.org/data/2.5/weather?q=${getInput()}&units=metric&appid=ab68c3c6730b171480e2ed2eeea340fc`
+        fetch(url)
     .then(res=>res.json())
-    .then(data=>displayWeather(data))
+            .then(data => displayWeather(data) )
 }
   
-getWeather();
+// getWeather();
 const displayWeather = (data)=>{
     const weather = document.getElementById('show-weather');
-    weather.innerHTML = `<p>Temperature: ${data.main.temp} C</br>
-  Visibilities: ${data.visibility/1000} K.M  </p>`
+    weather.innerHTML = `<p>Temperature: ${data.main.temp} &#8451;</br>
+    Feels Like : ${data.main.feels_like} &#8451;</br>
+  Visibilities: ${data.visibility / 1000} K.M</br>
+  Description: ${data.weather[0].description}  </p>
+  <i src="04d">icon</i>`
 }
