128ec5f3bb528d8a0747ab72756aade95603af31
joy biswas <contact.joybiswas@gmail.com>
2021-08-30 16:33:33 +0600

    simple Weather app done

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-function getInput() {
-    const input = document.getElementById('input');
+function getInput(id) {
+    const input = document.getElementById(id);
 const inputText = input.value;
     return inputText;
 }
 const getWeather = () => {
-    getInput();
-    let url =`https://api.openweathermap.org/data/2.5/weather?q=${getInput()}&units=metric&appid=ab68c3c6730b171480e2ed2eeea340fc`
-        fetch(url)
-    .then(res=>res.json())
-            .then(data => displayWeather(data) )
+    let url =`https://api.openweathermap.org/data/2.5/weather?q=${getInput('input')}&units=metric&appid=ab68c3c6730b171480e2ed2eeea340fc`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayWeather(data));
 }
   
 // getWeather();
 const displayWeather = (data)=>{
-    const weather = document.getElementById('show-weather');
+        const weather = document.getElementById('show-weather');
     weather.innerHTML = `<p>Temperature: ${data.main.temp} &#8451;</br>
     Feels Like : ${data.main.feels_like} &#8451;</br>
   Visibilities: ${data.visibility / 1000} K.M</br>
-  Description: ${data.weather[0].description}  </p>
-  <i src="04d">icon</i>`
+  Description: ${data.weather[0].description}  </p>`
+
 }
